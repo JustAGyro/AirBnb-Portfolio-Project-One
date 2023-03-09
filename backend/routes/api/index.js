e8545512f8afc4a1ec4442176500d1849d335f57
Kamran <kamran.zq@gmail.com>
2023-03-08 23:49:57 -0600

    added prettier formating extension

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,18 +2,8 @@
 const router = require('express').Router();
 
 //Test route
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+router.post('/test', function (req, res) {
+  res.json({ requestBody: req.body });
 });
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
