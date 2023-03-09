714929f9c5749b5b3a7fc381c4eb14126bb27925
Kamran <kamran.zq@gmail.com>
2023-03-08 22:39:20 -0600

    added api router / phase 1 done

---
+++ backend/routes/api/index.js
@@ -0,0 +1,19 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+//Test route
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 //Test route
 // router.get('/hello/world', function(req, res) {
@@ -16,6 +17,9 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
 });
+
+router.use('/api', apiRouter);
+
 
 
 module.exports = router;
