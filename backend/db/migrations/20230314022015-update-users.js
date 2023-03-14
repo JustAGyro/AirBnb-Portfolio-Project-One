2eb988be5ad9fdedb185cf2e8df6d9ab46342c63
Kamran <kamran.zq@gmail.com>
2023-03-13 22:07:23 -0500

    added options to queryinterfaces

--- backend/db/migrations/20230314022015-update-users.js
+++ backend/db/migrations/20230314022015-update-users.js
@@ -1,18 +1,34 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
-      type: Sequelize.DataTypes.STRING,
-    });
-    await queryInterface.addColumn('Users', 'lastName', {
-      type: Sequelize.DataTypes.STRING,
-    });
+    await queryInterface.addColumn(
+      'Users',
+      'firstName',
+      {
+        type: Sequelize.DataTypes.STRING,
+      },
+      options
+    );
+    await queryInterface.addColumn(
+      'Users',
+      'lastName',
+      {
+        type: Sequelize.DataTypes.STRING,
+      },
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    queryInterface.removeColumn('Users', 'firstName'),
-      queryInterface.removeColumn('Users', 'lastName');
+    options.tableName = 'Users';
+    queryInterface.removeColumn(options, 'firstName'),
+      queryInterface.removeColumn(options, 'lastName');
   },
 };
