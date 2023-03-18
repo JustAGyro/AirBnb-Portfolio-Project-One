416255220f05e94342086ca7052d0da4598b71c4
Kamran <kamran.zq@gmail.com>
2023-03-18 15:15:36 -0500

    fixing spots

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       SpotImage.belongsTo(models.Spot, {
-        foreignKey: 'spotId',
+        foreignKey: 'id',
       });
     }
   }
