db653c11742b04dedd5cb260b4da1ffaa10f8053
Kamran <kamran.zq@gmail.com>
2023-05-01 22:47:14 -0500

    auth me phase 0

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { restoreCSRF, csrfFetch } from './csrf';
 
 const rootReducer = combineReducers({
   // add reducer functions here
@@ -7,17 +8,26 @@ const rootReducer = combineReducers({
 
 let enhancer;
 
-if (process.env.NODE_ENV === "production") {
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+const store = configureStore();
+
+if (process.env.NODE_ENV === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require("redux-logger").default;
+  const logger = require('redux-logger').default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
-};
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 export default configureStore;
