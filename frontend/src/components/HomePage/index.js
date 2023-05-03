bc7ca4daa158ad3b1aff35cd6ed47c7b5c674a58
Kamran <kamran.zq@gmail.com>
2023-05-02 23:42:48 -0500

    added homepage and css stuff

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function fetchSpots() {
+      try {
+        const response = await fetch('/api/spots');
+        const data = await response.json();
+        setSpots(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchSpots();
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to my website!</h1>
-      <p>This is the homepage.</p>
+    <div className="spots-container">
+      {spots.map((spot) => (
+        <div key={spot.id} className="spot-card">
+          <img src="./images/image.jpg" alt={spot.name} />
+          <div className="spot-info">
+            <div className="spot-card-location">
+              <div className="spot-card-city">{spot.city},</div>
+              <div className="spot-card-state">{spot.state}</div>
+            </div>
+            <div className="average-rating">
+              <i className="fa fa-star"></i>
+              <div className="spot-card-stars">
+                {spot.average_rating.toFixed(1)}
+              </div>
+            </div>
+            <div className="spot-card-price">Price: ${spot.price}</div>
+          </div>
+          <Link to={`/spots/${spot.id}`}>
+            <h2>{spot.name}</h2>
+          </Link>
+          <div>{spot.description}</div>
+        </div>
+      ))}
     </div>
   );
 }
