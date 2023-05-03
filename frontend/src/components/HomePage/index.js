8736dcbbf783980a27f46f8b4aa0b8147545d499
Kamran <kamran.zq@gmail.com>
2023-05-02 22:42:25 -0500

    added homepage

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to my website!</h1>
+      <p>This is the homepage.</p>
+    </div>
+  );
+}
+
+export default HomePage;
